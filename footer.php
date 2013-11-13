31fcceb7439daacd53b2a07c0f85d1d60e140d5d
SaclyChan <saclychan@gmail.com>
2013-11-13 14:33:55 +0700

    Revert "submiy all, lỗi cmt, thay giao dien"
    This reverts commit c17decd52eb714a816e2a806c82d8fbed8411faa.

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
-﻿<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<div id="footer-information">
-		<H1><strong>SACLAONGS BAKERY</strong></H1>
-		<P>Designed by <i>unknown</P>
+		<p><strong>Website bán bánh trực tuyến</strong></p>
+		<p>Thiết kế: Nhóm 4 con mèo con - Đại học Bách Khoa Hà Nội</p>
 		<br />
-		<p style="font-size:12px"><strong>Tumbr:</strong> hungham2806 <br /><strong>Twitt:</strong>@hungham2806</p>
-		<span style="font-size:11px"><strong>Email: </strong><i>hungham2806@gmail.com - hungham2806@yahoo.com<br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;hungham2806@hotmail.com - 
-		hungham2806@msn.com</i>
+		<p style="font-size:12px"><strong>Phone:</strong> 0983 733 328 </p>
+		<span style="font-size:11px"><strong>Email: </strong><i>saclychan@gmail.com -  lamdt@familug.org<br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;phamjngocjlong@gmail.com - 
+		hungham2806@gmail.com</i>
 		</span>
 	</div>
 
